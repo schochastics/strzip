3d5987991205d75146243053cdd53e62469c35f3
schochastics <david@schochastics.net>
2024-10-05 13:46:45 +0200

    move init to new file

---
+++ src/init.c
@@ -0,0 +1,17 @@
+#include <R.h>
+#include <R_ext/Rdynload.h>
+#include <Rinternals.h>
+
+/* init */
+SEXP smaz_compress_R(SEXP input);
+SEXP smaz_decompress_R(SEXP input);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"smaz_compress_R", (DL_FUNC)&smaz_compress_R, 1},
+    {"smaz_decompress_R", (DL_FUNC)&smaz_decompress_R, 1},
+    {NULL, NULL, 0}};
+
+void R_init_Rsmaz(DllInfo *dll) {
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
--- src/wrappers.c
+++ src/wrappers.c
@@ -52,17 +52,3 @@ SEXP smaz_decompress_R(SEXP input) {
   UNPROTECT(1);
   return output;
 }
-
-/* init */
-SEXP smaz_compress_R(SEXP input);
-SEXP smaz_decompress_R(SEXP input);
-
-static const R_CallMethodDef CallEntries[] = {
-    {"smaz_compress_R", (DL_FUNC)&smaz_compress_R, 1},
-    {"smaz_decompress_R", (DL_FUNC)&smaz_decompress_R, 1},
-    {NULL, NULL, 0}};
-
-void R_init_Rsmaz(DllInfo *dll) {
-  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(dll, FALSE);
-}
